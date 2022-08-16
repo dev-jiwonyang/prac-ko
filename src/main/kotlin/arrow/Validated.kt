237b9619467a777a0eb9eed161b12e7060093144
dev-jiwonyang <jiwonyang@devsisters.com>
2022-08-16 18:05:01 +0900

    Validated

--- src/main/kotlin/arrow/Validated.kt
+++ src/main/kotlin/arrow/Validated.kt
@@ -1,4 +1,26 @@
 package arrow
 
-class Validated {
+import arrow.core.*
+
+fun main() {
+    val f = Validated.lift(String::toUpperCase, Int::inc)
+    val res1 = f("test".invalid())
+    println(res1)
+    val res2 = f(1.valid())
+    println(res2)
+
+    val result = "Hello".valid().void()
+    println(result)
+
+    Validated.Valid(12).tap { println("flower") }
+    Validated.Invalid(12).tapInvalid { println("flower") }
+
+    val validString: Validated<Int, String> = "Hello".valid()
+    val chars: Validated<Int, CharSequence> = validString.widen()
+    println(chars)
+
+    Validated.Valid(5).andThen {
+        println(it)
+        Validated.Valid(10)
+    }
 }
